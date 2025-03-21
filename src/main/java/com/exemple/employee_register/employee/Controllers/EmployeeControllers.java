1c76d800b1327b71b045c15468debf6079215cba
LimaDevCod3r <diegolima20100@gmail.com>
2025-03-21 05:44:11 -0300

    feat(employee): add CRUD for Employee

--- src/main/java/com/exemple/employee_register/employee/Controllers/EmployeeControllers.java
+++ src/main/java/com/exemple/employee_register/employee/Controllers/EmployeeControllers.java
@@ -2,11 +2,11 @@ package com.exemple.employee_register.employee.Controllers;
 
 import com.exemple.employee_register.employee.Models.EmployeeModel;
 import com.exemple.employee_register.employee.Services.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/employee")
@@ -20,7 +20,29 @@ public class EmployeeControllers {
     }
 
     @GetMapping
-    public List<EmployeeModel> getOneEmployee() {
-        return this.employeeService.FindAllEmployee();
+    public List<EmployeeModel> findAllEmployee() {
+        return this.employeeService.FindAll();
+    }
+
+
+    // @PathVariable recebe valores diretamente da URL como parâmetros
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeModel> findEmployeeById(@PathVariable UUID id) {
+        return employeeService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
+    @PostMapping
+    public EmployeeModel registerEmployee(@RequestBody EmployeeModel employee) {
+        return employeeService.create(employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable UUID id) {
+        employeeService.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/exemple/employee_register/employee/Services/EmployeeService.java
+++ src/main/java/com/exemple/employee_register/employee/Services/EmployeeService.java
@@ -5,6 +5,8 @@ import com.exemple.employee_register.employee.repositories.IEmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class EmployeeService {
@@ -14,11 +16,21 @@ public class EmployeeService {
 
     public EmployeeService(IEmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
-
     }
 
-    public List<EmployeeModel> FindAllEmployee() {
+    public List<EmployeeModel> FindAll() {
         return this.employeeRepository.findAll();
     }
 
+    public Optional<EmployeeModel> findById(UUID id){
+        return  this.employeeRepository.findById(id);
+    }
+
+    public EmployeeModel create(EmployeeModel employee){
+        return  this.employeeRepository.save(employee);
+    }
+
+    public void delete(UUID id){
+         this.employeeRepository.deleteById(id);
+    }
 }
