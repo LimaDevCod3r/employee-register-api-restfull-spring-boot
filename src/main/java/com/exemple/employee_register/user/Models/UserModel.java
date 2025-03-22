12d7fd96a8ed649ccc0c5536ffbb9ed094ce1cc0
LimaDevCod3r <diegolima20100@gmail.com>
2025-03-22 04:55:49 -0300

    feat: create User entity and add it to the database

--- src/main/java/com/exemple/employee_register/user/Models/UserModel.java
+++ src/main/java/com/exemple/employee_register/user/Models/UserModel.java
@@ -1,4 +1,63 @@
 package com.exemple.employee_register.user.Models;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+
+    @Column(unique = true,nullable = false)
+    public String email;
+
+    @Column(nullable = false)
+    public  String password;
+
+
+    public UserModel() {
+    }
+
+    public UserModel(UUID id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
